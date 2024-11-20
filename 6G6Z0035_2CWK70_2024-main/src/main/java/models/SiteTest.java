bbebe0e91f063b3a42c31fc33c10cd3dbccd65e7
Sourav Chowhan <sourav.chowhan@digigate360.com>
2024-11-21 01:47:26 +0600

    -_-

---
+++ 6G6Z0035_2CWK70_2024-main/src/main/java/models/SiteTest.java
@@ -0,0 +1,22 @@
+package models;
+
+import models.SiteSpec.TestSite;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SiteTest {
+
+    @Test
+    public void testGetLocation() {
+        // Arrange: Set up the location and create an instance of the test subclass
+        Location expectedLocation = Location.B;
+        Site testSite = new TestSite(expectedLocation);
+
+        // Act: Call the getLocation method
+        Location actualLocation = testSite.getLocation();
+
+        // Assert: Verify that the returned location matches the expected location
+        assertEquals("The getLocation method should return the correct location.", expectedLocation, actualLocation);
+    }
+}
